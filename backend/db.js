4e45e2db15714ed24f01a757b5e5a22fd790c455
Pavan393a <73477406+Pavan393a@users.noreply.github.com>
2025-09-16 23:10:55 +0100

    Update db.js
    Removed the link of mongodb url

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,15 @@ const model = require("mongoose");
 const mongoose = require("mongoose");
 const Schema = require("mongoose");
 // Connect to MongoDB
-mongoose.connect(
-  "mongodb+srv://saigopimorla:Saigopi8500@pavan.bqyui.mongodb.net/"
-);
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 const UserSchema = new mongoose.Schema({
   username: {
